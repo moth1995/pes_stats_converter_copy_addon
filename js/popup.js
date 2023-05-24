c599322520e1639c29883b0164901fb9695916da
moth1995 <marcos-flores@live.com.ar>
2023-05-24 04:37:58 -0300

    support for mobile browsers

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var redirectButton = document.getElementById('evoweb-button');
-    redirectButton.addEventListener('click', function() {
-      // Open a new tab
+  var redirectButton = document.getElementById('evoweb-button');
+  redirectButton.addEventListener('click', function() {
+    if (typeof chrome.tabs !== 'undefined') {
+      // Desktop browsers
       chrome.tabs.create({ url: 'https://evoweb.uk/threads/94290' });
-    });
+    } else if (typeof chrome.tabs.create === 'undefined') {
+      // Mobile browsers (e.g., Kiwi Browser, Yandex Browser)
+      window.open('https://evoweb.uk/threads/94290', '_blank');
+    }
   });
-  
+});
