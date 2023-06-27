565a1680efb8d86623e8b9b13053ba111510d880
moth1995 <marcos-flores@live.com.ar>
2023-06-27 16:56:41 -0300

    updating the popup to have a configuration tab

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var redirectButton = document.getElementById('evoweb-button');
-  redirectButton.addEventListener('click', function() {
-    if (typeof chrome.tabs !== 'undefined') {
-      // Desktop browsers
-      chrome.tabs.create({ url: 'https://evoweb.uk/threads/94290' });
-    } else if (typeof chrome.tabs.create === 'undefined') {
-      // Mobile browsers (e.g., Kiwi Browser, Yandex Browser)
-      window.open('https://evoweb.uk/threads/94290', '_blank');
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene el elemento select
+    const select = document.getElementById("selectOptionFMInside");
+    console.log("Elemento select encontrado:", select);
+  
+    // Obtiene la opción guardada en la variable global (si existe)
+    chrome.storage.local.get(["selectOptionFMInside"], function (result) {
+      // Si no se encuentra la opción guardada, se establece como predeterminada "pes5"
+      const selectedOption = result.selectOptionFMInside || "pes5";
+      
+      // Establece la opción seleccionada en el select
+      select.value = selectedOption;
+    });
+  
+    // Maneja el evento de cambio del select
+    select.addEventListener("change", function () {
+      const selectedValue = select.value;  
+      // Guarda la opción seleccionada en el almacenamiento local
+      chrome.storage.local.set({ selectOptionFMInside: selectedValue }, function () {
+        console.log("Valor guardado en el almacenamiento local");
+      });
+    });
 });
+  
+document.addEventListener("DOMContentLoaded", function () {
+const tabs = document.querySelectorAll(".tab");
+const tabButtons = document.querySelectorAll(".tab-button");
+tabButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+    const targetTab = button.getAttribute("data-tab");
+    console.log(targetTab);
+    tabs.forEach(function (tab) {
+        console.log(tab.id);
+        if (tab.id === targetTab) {
+        tab.classList.add("active");
+        } else {
+        tab.classList.remove("active");
+        }
+    });
+    });
+});
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+var redirectButton = document.getElementById('evoweb-button');
+redirectButton.addEventListener('click', function() {
+    if (typeof chrome.tabs !== 'undefined') {
+    // Desktop browsers
+    chrome.tabs.create({ url: 'https://evoweb.uk/threads/94290' });
+    } else if (typeof chrome.tabs.create === 'undefined') {
+    // Mobile browsers (e.g., Kiwi Browser, Yandex Browser)
+    window.open('https://evoweb.uk/threads/94290', '_blank');
+    }
+});
+});
+
