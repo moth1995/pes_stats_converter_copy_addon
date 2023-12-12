0e42bd29556c9f9eb7f00c75d83d9fccb7941a82
moth1995 <marcos-flores@live.com.ar>
2023-12-12 15:49:15 -0300

    adding privacy policy to the popup

--- js/popup.js
+++ js/popup.js
@@ -41,7 +41,7 @@ tabButtons.forEach(function (button) {
 });
 });
 
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 var redirectButton = document.getElementById('evoweb-button');
 redirectButton.addEventListener('click', function() {
     if (typeof chrome.tabs !== 'undefined') {
@@ -54,3 +54,17 @@ redirectButton.addEventListener('click', function() {
 });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var redirectButton = document.getElementById('privacy-button');
+  redirectButton.addEventListener('click', function() {
+      if (typeof chrome.tabs !== 'undefined') {
+      // Desktop browsers
+      chrome.tabs.create({ url: 'policy_privacy.html' });
+      } else if (typeof chrome.tabs.create === 'undefined') {
+      // Mobile browsers (e.g., Kiwi Browser, Yandex Browser)
+      window.open('policy_privacy.html', '_blank');
+      }
+  });
+  });
+  
+  
